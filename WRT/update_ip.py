1ed1038133b5ddc51d87d716216c45ed5ec94454
quadrupleflame <harrisonhuang1025@gmail.com>
2019-04-30 15:22:29 -0400

    fix sql injection problem

--- WRT/update_ip.py
+++ WRT/update_ip.py
@@ -16,15 +16,16 @@ def update_device_domains(device_dict):
     name = device_dict['mac_address']
     domain = device_dict['domains'][0]['domain'][:-1]
 
-    query = "SELECT NAME, HOSTNAME, DOMAIN, IP, PORT, PROTOCOL from DEVICE WHERE NAME = " + "'{0}'".format(name) + " AND DOMAIN = " + "'{0}'".format(domain)
-    answer = cursor.execute(query)
+    
+    query = "SELECT NAME, HOSTNAME, DOMAIN, IP, PORT, PROTOCOL from DEVICE WHERE NAME = ? AND DOMAIN = ?"
+    cursor.execute(query, (name, domain))
 
     ipList = []
     port = ''
     protocol = ''
     hostName = ''
 
-    rules = answer.fetchall()
+    rules = cursor.fetchall()
     if len(rules):
         print "[INFO] " + name + " " + str(len(rules)) + " IPs for domain " + domain + " in the database"
     if rules:
@@ -48,8 +49,8 @@ def update_iptable(ipList, newIpList, port, protocol, domain, mac_addr, hostName
     # clear databse for the domain
     conn = sqlite3.connect('device.db')
     cursor = conn.cursor()
-    old_query = "DELETE FROM DEVICE WHERE NAME = '{0}' AND DOMAIN = '{1}'".format(mac_addr, domain)
-    cursor.execute(old_query)
+    old_query = "DELETE FROM DEVICE WHERE NAME = ? AND DOMAIN = ?"
+    cursor.execute(old_query, (mac_addr, domain))
     conn.commit()
 
     # remove old IP from iptables
@@ -63,8 +64,8 @@ def update_iptable(ipList, newIpList, port, protocol, domain, mac_addr, hostName
         call('iptables -I FORWARD -p ' + protocol + ' -d '+ newIp + ' --dport ' + port + ' -m mac --mac-source ' + mac_addr + ' -j ACCEPT', shell=True)
         print "[INFO] Add new IP {0} to iptables".format(newIp)
         # database
-        query = "INSERT INTO DEVICE(NAME, HOSTNAME, DOMAIN, IP, PORT, PROTOCOL) VALUES('{0}','{1}','{2}','{3}','{4}', '{5}')".format(mac_addr, hostName, domain, newIp, port, protocol)
-        cursor.execute(query)
+        query = "INSERT INTO DEVICE(NAME, HOSTNAME, DOMAIN, IP, PORT, PROTOCOL) VALUES(?, ?, ?, ?, ?, ?)"
+        cursor.execute(query, (mac_addr, hostName, domain, newIp, port, protocol))
         conn.commit()
         print "[INFO] Add new IP {0} to database".format(newIp)
     
@@ -76,14 +77,16 @@ def get_domain_list(pkt):
     except:
         print "[ERROR] Fail to connect to database"
         return []
-    
+
     cursor = conn.cursor()
 
-    query = "SELECT DOMAIN FROM DEVICE WHERE NAME = '{0}'".format(mac_addr)
-    answer = cursor.execute(query)
-
+    query = "SELECT DOMAIN FROM DEVICE WHERE NAME = ?"
+    
+    cursor.execute(query, (mac_addr, ))
+    answer = cursor.fetchall()
     res = []
-    for domain in answer.fetchall():
+    # for domain in answer.fetchall():
+    for domain in answer:
         res.append(domain)
     
     return list(set(res))
