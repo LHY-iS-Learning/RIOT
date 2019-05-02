4d3bd29a1bbeac846b07ff7d99261124a559c9ea
quadrupleflame <harrisonhuang1025@gmail.com>
2019-05-02 14:57:04 -0400

    update send_email

--- WRT/send_email.py
+++ WRT/send_email.py
@@ -10,7 +10,6 @@ def alert(useraddr):
     # password = raw_input('Type in your email password:')
     #password = getpass.getpass("Type in your email password: ")
     password = '1234567890s'
-    #sender = 'harrisonhuang1025@gmail.com' 
     sender = 'lhyemailsender@gmail.com'
     server.login(sender, password)
 
@@ -18,10 +17,43 @@ def alert(useraddr):
     <html>
         <h1>FAIL to find a MUD for this device</h1>
         <p> Go to the router address </p>
-        <a href = 'http://192.168.2.1:5000/'> My router </a>
+        <a href = 'http://192.168.2.1:5000/'> My router: http://192.168.2.1:5000/ </a>
     </html>
     '''
     # msg = MIMEText('FAIL to find a MUD for this device', 'plain', 'utf-8')
+    msg = MIMEText(message, 'html')
+
+    msg['Subject'] = Header('Alert from router', 'utf-8')
+    msg['From'] = sender
+    msg['To'] = useraddr
+    try:
+        server.sendmail(sender, useraddr, msg.as_string())
+        print 'email sended'
+    except:
+        print 'fail to send email'
+    finally:
+        server.quit()
+
+
+
+def case_two_alert(useraddr, matches):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    # password = raw_input('Type in your email password:')
+    #password = getpass.getpass("Type in your email password: ")
+    password = '1234567890s'
+    sender = 'lhyemailsender@gmail.com'
+    server.login(sender, password)
+
+    message = '''\
+    <html>
+       <h1> Hi, we find a similar mud file with similarity {match} </h1>
+       <p> Go to ther router address </p>
+       <a href = 'http://192.168.2.1:5000/'> My router http://192.168.2.1:5000/ </a>
+    </html>
+    '''.format(match = matches)
+
     msg = MIMEText(message, 'html')
 
     msg['Subject'] = Header('Alert from router', 'utf-8')
